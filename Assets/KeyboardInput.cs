abf509a523c08e4aa77d7dd28a734c78379cf626
Nick Mudry <nick@mudry.me>
2014-11-04 16:11:28 -0500

    KeyboardInput For GadgetSpawning
    Allows spawning of gadgets using keyboard keys.
    
    QWE currently spawn Fire, Oil and Gravity, in that order.
    
    Attach to ... any object. I had attached to the UICamera, but it doesn't
    really matter I think since it is calling that script off of it.

---
+++ Assets/KeyboardInput.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyboardInput : MonoBehaviour {
+    PlaceGadget GadgetSpawner;
+	// Use this for initialization
+	void Start () {
+        GadgetSpawner = GameObject.Find("tk2dUICamera").GetComponent<PlaceGadget>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown(KeyCode.Q))
+        {
+            GadgetSpawner.SpawnFire();
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            GadgetSpawner.SpawnOil();
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            GadgetSpawner.SpawnReverseGravity();
+        }
+	}
+}
